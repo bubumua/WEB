21c5e1b5f122085aaf24e355993c774f81c714da
bubu <1069785399@qq.com>
2023-06-12 23:21:58 +0800

    update files

--- GPT/GPT-1/GPT_1.js
+++ GPT/GPT-1/GPT_1.js
@@ -1,9 +1,9 @@
 const url_anwei = "https://v.api.aa1.cn/api/api-wenan-anwei/index.php?type=json";
 const url_gpt_v1 = 'https://v1.apigpt.cn/';
+const url_gpt4_sealos_forefront = "https://rqdmrenzaeao.cloud.sealos.io/ask?model=forefront&prompt=";
 const messagesContainer = document.querySelector('.messages');
 const messageInput = document.querySelector('#message-input');
 const sendButton = document.querySelector('#send-button');
-
 function sendMessage() {
     const message = messageInput.value.trim();
     if (message === '') {
@@ -18,8 +18,13 @@ function sendMessage() {
     // create div for message to receive
     const messageReceiveElement = document.createElement('div');
     messageReceiveElement.classList.add('message', 'received');
+
+    // generate url to send
     var question = $('#message-input').val();
-    var url_send = url_gpt_v1 + '?q=' + question + '&apitype=sql';
+    let url_send = url_gpt4_sealos_forefront + question;
+    url_send = url_gpt_v1 + '?q=' + question + '&apitype=sql';
+
+    // send and receive response
     $.getJSON(url_send, function (data) {
         messageReceiveElement.textContent = data.ChatGPT_Answer;
     });
